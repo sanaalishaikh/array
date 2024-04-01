1b883b51c38dbaa432f72804b2b9624d94275ab8
sanaalishaikh <162053739+sanaalishaikh@users.noreply.github.com>
2024-04-02 02:45:49 +0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,19 @@
+//   var countries =["India", "USA", "Japan", "Russia"];
+
+//     var states = new Array("Rajhasthan", "Delhi","Mumbai","Assam")
+
+//     console.log(states.length);
+//     states[0] = "punjab";
+//     console.log(states);
+var user = ["hitesh", "hitesh@lco.dev", 3, 34,true];
+console.log(user);
+
+user.pop();
+user.pop();
+console.log(user);
+user.unshift("NEW VALUE");
+console.log(user);
+user.shift();
+console.log(user);
+
+
